d5012b360fcadcfdadb4c435f94090a7c5fee4f3
Joyee Cheung <joyeec9h3@gmail.com>
2014-11-08 08:39:42 +0800

    rearrange && add draggable

---
+++ ui/slide-and-expand/script.js
@@ -0,0 +1,27 @@
+;(function(d, undefined) {
+    var btn = d.getElementById('btn');
+    btn.addEventListener('click', function(e) {
+        e.preventDefault(); // stop submit
+
+        // clean up input
+        var input = d.getElementById('new-comment-text');
+        var value = input.value;
+        input.value = "";
+
+        // create a new comment div
+        var comment = d.createElement('div');
+        var para = d.createElement('p');
+        para.textContent = value;
+        comment.appendChild(para);
+        comment.className = "comment hide";
+
+        // put the new comment into DOM
+        var comments = d.getElementById('old-comment')
+        comments.insertBefore(comment, comments.firstChild);
+
+        // need a timeout for the transition to show
+        setTimeout(function() {
+            comment.className = "comment";
+        }, 10);
+    });
+})(document);
