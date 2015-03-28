8744a4f0e52932a85e1c1aa1ba455a735ea5efbc
Joyee Cheung <joyeec9h3@gmail.com>
2015-03-28 23:16:57 +0800

    promise

--- js/promise/util.js
+++ js/promise/util.js
@@ -1,58 +1,67 @@
-;var util = (function(){
-    function MockData() {
-        var mockName = ["John", "Doe", "Alice", "Simpson", "Peter", "Tom"];
-        var mocNumber = ["12335221", "34591831", "423411134", "34240131", "42341231"];
+;var util = (function() {
+  'use strict';
 
-        function select(iterable) {
-            var idx = Math.floor(Math.random() * iterable.length);
-            return iterable[idx];
-        }
+  /***************** Mock data ********************/
+  function mockData() {
+    var mockName = ["John", "Doe", "Alice", "Simpson", "Peter", "Tom"];
+    var mocNumber = ["12335221", "34591831", "423411134", "34240131", "42341231"];
 
-        return {
-            get: function() {
-                return {
-                    name: select(mockName),
-                    number: select(mocNumber)
-                };
-            }
-        };
-    }
-
-    function $http(url) {
-        function ajaxPromise(resolve, reject) {
-            var mock = MockData();
-            var sometime = Math.random() * 2 * 1000;  // 0 ~ 2s
-            setTimeout(function() {
-                var prob = Math.random();
-                if (prob < 0.8) {
-                    resolve(mock.get());
-                } else {
-                    reject({"error": "server error"});
-                }
-            }, sometime);
-        }
-
-        function ajax(method, url, args) {
-            return new Promise(ajaxPromise);
-        }
-
-        return {
-            'get': function(args) {
-                return ajax('GET', url, args);
-            },
-            'post': function(args) {
-                return ajax('POST', url, args);
-            },
-            'put': function(args) {
-                return ajax('PUT', url, args);
-            },
-            'delete': function(args) {
-                return ajax('DELETE', url, args);
-            }
-        };
+    function select(iterable) {
+      var idx = Math.floor(Math.random() * iterable.length);
+      return iterable[idx];
     }
 
     return {
-        $http: $http
+      get: function() {
+        return {
+          name: select(mockName),
+          number: select(mocNumber)
+        };
+      }
     };
+  }
+
+  /***************** Promisified ajax ********************/
+  function $http(url) {
+
+    function ajaxPromise(resolve, reject) {
+      var mock = mockData();
+      var sometime = Math.random() * 2 * 1000; // 0 ~ 2s
+      setTimeout(function() {
+        var prob = Math.random();
+        if (prob < 0.8) {
+          resolve(mock.get());
+        } else {
+          reject({
+            "error": "server error"
+          });
+        }
+      }, sometime);
+    }
+
+    function ajax(method, url, args) {
+      return new Promise(ajaxPromise);
+    }
+
+    return {
+      'get': function(args) {
+        return ajax('GET', url, args);
+      },
+      'post': function(args) {
+        return ajax('POST', url, args);
+      },
+      'put': function(args) {
+        return ajax('PUT', url, args);
+      },
+      'delete': function(args) {
+        return ajax('DELETE', url, args);
+      }
+    };
+  }
+
+  /******* Exposed API of this module ******/
+  return {
+    $http: $http
+  };
+
 }());
